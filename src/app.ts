b8731c10ac716037b632652a5d1f81e022616d93
sofia <sofia@blockinar.com>
2024-09-04 14:41:47 -0300

    CORS

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express, { json } from "express";
+import cors from "cors";
 import indexRouter from "./routes";
 import errorHandler from "./middlewares/error-handler";
 
 const app = express();
+
+// Configura CORS para permitir acceso desde tu frontend
+const corsOptions = {
+  origin: "http://localhost:5173", // Reemplaza con la URL de tu frontend
+  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+  credentials: true, // Si necesitas compartir cookies
+};
+
+app.use(cors(corsOptions));
 
 app.use(json());
 app.use("/status", (req, res) =>
